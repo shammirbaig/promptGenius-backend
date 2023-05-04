2d7daaa3af3552a4ec5319a50147abdf97b824c3
Shammir Baig <mshammirbaig@gmail.com>
2023-05-04 10:08:16 +0530

    added cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 app.use(express.json())
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "http://127.0.0.1:5500"||"https://the-promptgenius.com",
   })
 )
 const port = process.env.PORT || 3000;
